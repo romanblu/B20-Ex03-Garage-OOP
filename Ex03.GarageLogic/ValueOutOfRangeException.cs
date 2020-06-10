50272de08542a259dffba239808b3f1a4d01fba7
Inbal Gozland <Inbal.Gozland@post.idc.ac.il>
2020-06-10 19:42:19 +0300

    value exception class

--- Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -10,5 +10,26 @@ namespace Ex03.GarageLogic
     {
         private float MaxValue;
         private float MinValue;
+        public float MaxValueGet {get;} 
+        public float MinValueGet {get;}
+
+        public ValueOutOfRangeException()
+        {
+
+        }
+
+        public ValueOutOfRangeException(string message) : base(message)
+        {
+
+        }
+
+        public ValueOutOfRangeException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+        public ValueOutOfRangeException(string message, float i_MaxValue, float i_MinValue) : this(message)
+        {
+
+        } 
     }
 }
