d56620db8e31383c063095578099a7ec505565a3
roman2702rb <roman2702rb@gmail.com>
2020-06-11 00:43:04 +0300

    b

--- Ex03.GarageLogic/CeateNewVehicle.cs
+++ Ex03.GarageLogic/CeateNewVehicle.cs
@@ -11,28 +11,28 @@ namespace Ex03.GarageLogic
         private eVehicleType vehicle;
         
         public Vehicle CreateVehicle(string i_ModelName, string i_LicensePlate, float i_EnergyLeft,
-            string i_VehicleType, List<object> i_VehicleExtraData)
+            string i_VehicleType, List<string> i_VehicleExtraData)
         {
             Vehicle newVehicle = null;
             if (i_VehicleType == "Car")
             {
-                newVehicle = new Car(i_ModelName, i_LicensePlate, i_EnergyLeft,  i_VehicleExtraData[0], i_VehicleExtraData[1]);
+                newVehicle = new Car(i_ModelName, i_LicensePlate, i_EnergyLeft,  i_VehicleExtraData[0], int.Parse(i_VehicleExtraData[1]));
             }
             else if (i_VehicleType == "Electric Car")
             {
-                newVehicle = new ElectricCar(i_ModelName, i_LicensePlate, i_EnergyLeft,  i_VehicleExtraData[0], i_VehicleExtraData[1]);
+                newVehicle = new ElectricCar(i_ModelName, i_LicensePlate, i_EnergyLeft,  i_VehicleExtraData[0], int.Parse(i_VehicleExtraData[1]));
             }
             else if (i_VehicleType == "Motorcycle")
             {
-                newVehicle = new Motorcycle();
+                newVehicle = new Motorcycle(i_ModelName, i_LicensePlate, i_EnergyLeft, i_VehicleExtraData[0], int.Parse(i_VehicleExtraData[1]));
             }
             else if (i_VehicleType == "Electric Motorcycle")
             {
-                newVehicle = new ElectricMotorcycle();
+                newVehicle = new ElectricMotorcycle(i_ModelName, i_LicensePlate, i_EnergyLeft, i_VehicleExtraData[0], int.Parse(i_VehicleExtraData[1]));
             }
             else if (i_VehicleType == "Truck")
             {
-                newVehicle = new Truck();
+                newVehicle = new Truck(i_ModelName, i_LicensePlate, i_EnergyLeft,bool.Parse( i_VehicleExtraData[0]), float.Parse(i_VehicleExtraData[1]));
             }
             else
             {
